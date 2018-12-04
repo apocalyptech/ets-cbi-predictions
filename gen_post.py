57246f68857e25eb561b190869b1e9632df158b5
CJ Kucera <pez@apocalyptech.com>
2018-12-04 15:49:07 -0600

    No new tracks on the Dec 3 show

--- gen_post.py
+++ gen_post.py
@@ -4,14 +4,6 @@
 import sys
 import subprocess
 
-if len(sys.argv) != 2:
-    print('Need a number-of-shows-missed count')
-    sys.exit(1)
-missed = int(sys.argv[1])
-
-# Calculate no-debut vote points
-points_no_debut = max(0, 100-(10*missed))
-
 # Get score output
 score_text = subprocess.run(
         ['./scores.py'],
@@ -25,5 +17,5 @@ with open('post.bbcode.in', 'r') as df:
             if line.strip() == '{{scores}}':
                 out.write(score_text)
             else:
-                out.write(line.replace('{{points_no_debut}}', str(points_no_debut)))
+                out.write(line)
 print('Wrote to post.bbcode')
